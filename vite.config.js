25384f93a320cd46f0b4abdb0544d7155bfd3f8d
shivansh mehrotra <shivaevilking@gmail.com>
2025-08-24 01:00:10 +0530

    updated the build logic in index file

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
 // vite.config.js
-import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Myportfolio/', // Set '/' for root domain deployment, or '/your-subfolder/' if deploying under a subpath
+  base: '/', // Set '/' for root domain deployment, or '/your-subfolder/' if deploying under a subpath
 });
